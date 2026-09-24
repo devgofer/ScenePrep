04169f956d5f2854a891f39c9fdbcbe56f8f7a1c
Nina Weng <orxbis@gmail.com>
2026-09-24 17:22:49 +0800

    Test Apple TV episode URL parsing

---
+++ tests/test_apple_tv.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import pytest
+
+from scene_prep.apple_tv import AppleTVSubtitleProvider, parse_apple_tv_episode
+
+
+URL = (
+    "https://tv.apple.com/tw/episode/%E8%A9%A6%E6%92%AD%E9%9B%86/"
+    "umc.cmc.zb0yksqtym68hasbq8mj4jwp"
+    "?showId=umc.cmc.vtoh0mn0xn7t3c643xqonfzy"
+    "&playableId=tvs.sbd.4000%3AVTEDL0560101"
+)
+
+
+def test_parse_apple_tv_episode():
+    episode = parse_apple_tv_episode(URL)
+
+    assert episode.episode_id == "umc.cmc.zb0yksqtym68hasbq8mj4jwp"
+    assert episode.show_id == "umc.cmc.vtoh0mn0xn7t3c643xqonfzy"
+    assert episode.playable_id == "tvs.sbd.4000:VTEDL0560101"
+
+
+def test_reject_non_apple_tv_url():
+    with pytest.raises(ValueError, match="Not an Apple TV URL"):
+        parse_apple_tv_episode("https://example.com/episode/123")
+
+
+def test_provider_does_not_bypass_access_controls():
+    episode = parse_apple_tv_episode(URL)
+    provider = AppleTVSubtitleProvider()
+
+    with pytest.raises(NotImplementedError, match="cannot bypass DRM"):
+        provider.resolve_subtitle_url(episode)
